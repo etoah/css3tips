70fb3b520204d34fb2dea30e17523ef2598e2b66
etoah <etoahn@163.com>
2015-09-09 07:28:50 +0800

    对复杂的应用添加脚本

---
+++ js/tips.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Lucien on 9/9/2015.
+ */
+;(function(window,document){
+    var tips=document.querySelectorAll('.l-tips'),
+        contents=document.querySelectorAll('.l-tips p');
+
+    
+
+
+})(window,document);
