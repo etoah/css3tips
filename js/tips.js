32288d31987a97a3bfde8d4a8de89a9a15bb3188
etoah <etoahn@163.com>
2015-09-10 22:26:16 +0800

    添加js文件

--- js/tips.js
+++ js/tips.js
@@ -1,11 +1,27 @@
 /**
  * Created by Lucien on 9/9/2015.
  */
-;(function(window,document){
-    var tips=document.querySelectorAll('.l-tips'),
-        contents=document.querySelectorAll('.l-tips p');
+;
+//(function (window, document) {
 
-    
+    var TIPS_CLASSNAME='.l-tips';
+
+    function nextElement(ele) {
+        var next =ele.nextSibling;
+        while (next && next.nodeType !== 1) {
+            next = ele.nextSibling;
+        }
+        return next;
+
+    }
+
+    function setStyle(ele)
+    {
+        
+    }
+
+    var tips = document.querySelectorAll(TIPS_CLASSNAME);
 
 
-})(window,document);
+
+//})(window, document);
